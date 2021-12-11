f17a127e993ec3f26b3ce9fb01a7c1617b7da31c
restitux <restitux@ohea.xyz>
2021-12-10 23:11:30 -0700

    Day 11 cleanup

--- 11/part2/src/main.rs
+++ 11/part2/src/main.rs
@@ -2,15 +2,6 @@ use std::fs;
 
 use eyre::Result;
 
-//fn error_val(c: &str) -> usize {
-//    match c {
-//        ")" => 3,
-//        "]" => 57,
-//        "}" => 1197,
-//        ">" => 25137,
-//        _ => panic!("Invalid char"),
-//    }
-//}
 
 struct Octopus {
     energy: usize,
@@ -40,7 +31,6 @@ fn main() -> Result<()> {
 
     let mut t = 0;
 
-    //for t in 0..100 {
     loop {
         t = t + 1;
         println!("t: {}", t);
@@ -103,96 +93,6 @@ fn main() -> Result<()> {
         }
         total_flashes = total_flashes + iteration_flashes;
     }
-
-    println!("TOTAL FLASHES: {}", total_flashes);
-
-    //let mut error_score = 0;
-
-    //for i in 0..lines.len() {
-    //    // for each line
-    //    let mut stack: Vec<String> = Vec::new();
-    //    for j in 0..lines[i].len() {
-    //        match lines[i][j].as_str() {
-    //            "(" => stack.push(lines[i][j].clone()),
-    //            "[" => stack.push(lines[i][j].clone()),
-    //            "{" => stack.push(lines[i][j].clone()),
-    //            "<" => stack.push(lines[i][j].clone()),
-    //            ")" => {
-    //                match stack.pop() {
-    //                    Some(c) => match c.as_str() {
-    //                        "(" => (),//println!("FOUND MATCH"),
-    //                        _ => {
-    //                            println!("ERROR");
-    //                            error_score = error_score + error_val(")");
-    //                        },
-    //                    },
-    //                    None => continue,
-    //                }
-    //            },
-    //            "]" => {
-    //                match stack.pop() {
-    //                    Some(c) => match c.as_str() {
-    //                        "[" => (),//println!("FOUND MATCH"),
-    //                        _ => {
-    //                            println!("ERROR");
-    //                            error_score = error_score + error_val("]");
-    //                        },
-    //                    },
-    //                    None => continue,
-    //                }
-    //            },
-    //            "}" => {
-    //                match stack.pop() {
-    //                    Some(c) => match c.as_str() {
-    //                        "{" => (),//println!("FOUND MATCH"),
-    //                        _ => {
-    //                            println!("ERROR");
-    //                            error_score = error_score + error_val("}");
-    //                        },
-    //                    },
-    //                    None => continue,
-    //                }
-    //            },
-    //            ">" => {
-    //                match stack.pop() {
-    //                    Some(c) => match c.as_str() {
-    //                        "<" => (),//println!("FOUND MATCH"),
-    //                        _ => {
-    //                            println!("ERROR");
-    //                            error_score = error_score + error_val(">");
-    //                        },
-    //                    },
-    //                    None => continue,
-    //                }
-    //            },
-    //            _ => panic!("UNKNOWN CHAR"),
-    //        }
-    //        print!("{}", lines[i][j]);
-    //    }
-    //    println!("");
-    //}
-
-    //println!("ERROR SCORE: {}", error_score);
-
-    //let mut sum = 0;
-
-    //for i in 0..map.len() {
-    //    for j in 0..map[i].len() {
-    //        if i == 0 || map[i][j] < map[i - 1][j] {
-    //            if j == 0 || map[i][j] < map[i][j - 1] {
-    //                if i == (map.len() - 1) || map[i][j] < map[i + 1][j] {
-    //                    if j == (map[i].len() - 1) || map[i][j] < map[i][j + 1] {
-    //                        println!("FOUND HOLE");
-    //                        sum = sum + 1 + map[i][j];
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    }
-    //}
-
-    //println!("SUM: {}", sum);
-
 
     Ok(())
 }
